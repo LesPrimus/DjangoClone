291d11d6a5a8e9abe96d7e23b231f947fd0763cc
LesPrimus <amerigo.armentano@hotmail.it>
2020-12-16 20:15:04 +0100

    add tests

--- django_clone_helper/tests.py
+++ django_clone_helper/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from .models import Artist, Album, Song, Compilation
+
+
+@pytest.fixture
+def artist(db):
+    artist = Artist.objects.create(name='Les')
+    return artist
+
+
+@pytest.fixture
+def album(artist, db):
+    album = Album.objects.create(title='Frizzle Fry', artist=artist)
+    return album
+
+
+@pytest.fixture
+def song(artist, album, db):
+    song = Song.objects.create(
+        title='My name is mud', album=album, artist=artist
+    )
+    return song
+
+
+@pytest.mark.djangp_db
+class TestSuite:
+
+    def test_clone_parent_model(self, artist):
+        attrs = {'name': 'John'}
+        cloned_artist = artist.clone.create_child(attrs=attrs)
+        assert Artist.objects.count() == 2
+        assert cloned_artist.name == attrs.get('name')
