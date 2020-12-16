0a81f57052391df055545fde416a5f18f3d31d31
LesPrimus <amerigo.armentano@hotmail.it>
2020-12-16 19:52:05 +0100

    Add models

--- django_clone_helper/models.py
+++ django_clone_helper/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=100)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Compilation(models.Model):
+    title = models.CharField(max_length=100)
+    songs = models.ManyToManyField(Song)
+
+    def __str__(self):
+        return self.title
