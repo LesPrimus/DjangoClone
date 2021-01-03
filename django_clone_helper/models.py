1d7f6a2d4673b544ea24f77a63fb0bbee032dbd3
LesPrimus <amerigo.armentano@hotmail.it>
2021-01-03 15:13:14 +0100

    Add GenericFK model

--- django_clone_helper/models.py
+++ django_clone_helper/models.py
@@ -1,5 +1,7 @@
 from uuid import uuid4
 
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 from django_clone_helper.helpers import CloneHandler
@@ -105,6 +107,16 @@ class Instrument(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class TaggedItem(models.Model):
+    tag = models.SlugField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    def __str__(self):
+        return self.tag
 
 
 class BassGuitar(Instrument):
